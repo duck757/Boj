ecdbe1d0c7d4fedd50136424dca302d961066fff
duck757 <maverickwillson4@gmail.com>
2025-07-22 13:24:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function startRandomCountingLoop() {
   const channel = await client.channels.fetch(CHANNEL_ID);
 
   while (true) {
-    const waitTime = randInt(3, 25) * 60 * 1000;
+    const waitTime = randInt(15, 25) * 60 * 1000;
     logStatus("Sleeping", `Waiting ${Math.floor(waitTime / 60000)} mins`);
     await sleep(waitTime);
 
